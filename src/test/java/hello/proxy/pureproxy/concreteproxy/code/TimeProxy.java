580153b129fe446e2cfdd82f32777b76e8de2b23
lee <anomalocaris3871@gmail.com>
2022-04-01 20:51:03 +0900

    Add concrete proxy test

---
+++ src/test/java/hello/proxy/pureproxy/concreteproxy/code/TimeProxy.java
@@ -0,0 +1,29 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeProxy extends ConcreteLogic {
+
+    private ConcreteLogic concreteLogic;
+
+    public TimeProxy(ConcreteLogic concreteLogic) {
+        this.concreteLogic = concreteLogic;
+    }
+
+    @Override
+    public String operate() {
+
+        log.info("TimeDecorator 실행");
+        long startTime = System.currentTimeMillis();
+
+        String result = concreteLogic.operate();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+
+        log.info("TimeDecorator 종료 resultTime={}ms", resultTime);
+
+        return result;
+    }
+}
